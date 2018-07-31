cfb29b2411c0ad2d4b4992aeb9ea0f6317a87f23
Nikki A <nikki.apostolakis@gmail.com>
2018-07-31 21:42:10 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -21,5 +21,5 @@ def select_series_title_with_most_human_characters
 end
 
 def select_character_names_and_number_of_books_they_are_in
-  "SELECT characters.name, COUNT(character_books.character_id) FROM characters LEFT OUTER JOIN character_books ON character_books.character_id=characters.id GROUP BY characters.id"
+  "SELECT characters.name, COUNT(character_books.character_id) FROM characters LEFT OUTER JOIN character_books ON character_books.character_id=characters.id GROUP BY characters.id ORDER BY characters.name, COUNT(character_books.character_id) DESC"
 end
