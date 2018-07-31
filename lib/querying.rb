9dd284e20271aa19e3e62c323274ce39c3974d50
Nikki A <nikki.apostolakis@gmail.com>
2018-07-31 21:23:34 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -16,7 +16,8 @@ def select_name_and_series_subgenres_of_authors
 end
 
 def select_series_title_with_most_human_characters
-  "SELECT series.title FROM series LEFT OUTER JOIN characters ON series.id=characters.series_id Group By characters.species"
+  #{}"SELECT series.title FROM series LEFT OUTER JOIN characters ON series.id=characters.series_id Group By characters.species"
+  "SELECT series.title FROM characters WHERE characters.species='human' LEFT OUTER JOIN series ON series.id=characters.series_id"
 end
 
 def select_character_names_and_number_of_books_they_are_in
