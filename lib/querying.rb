5f7819574e9fb5e3371d4f1d73c6262665ef4561
Nikki A <nikki.apostolakis@gmail.com>
2018-07-31 21:37:49 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -21,5 +21,5 @@ def select_series_title_with_most_human_characters
 end
 
 def select_character_names_and_number_of_books_they_are_in
-  "Write your SQL query here"
+  "SELECT characters.name FROM characters LEFT OUTER JOIN character_books ON character_books.character_id=characters.id"
 end
